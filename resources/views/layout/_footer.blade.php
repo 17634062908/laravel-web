b4857375080b8254e7312e24c2063fa1767f3f2a
jesse <851612652@qq.com>
2020-12-02 16:58:24 +0800

    分离头尾视图

---
+++ resources/views/layout/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/15x15" />
+    <a href="https://laravel-china.org/courses" target=_blank>
+        刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="/index.php/about" >关于</a>
+    </div>
+</footer>
